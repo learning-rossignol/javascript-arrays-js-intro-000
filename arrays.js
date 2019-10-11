c7725f78865bd50f7c34f2b014d0dfa88f5107cd
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 18:33:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,6 @@ destructivelyRemoveElementFromEndOfArray = (arr) => {
   return arr;
 }
 removeElementFromEndOfArray = (arr) => arr.slice(0, arr.length - 1);
+
+myArray = [1,2,3];
+console.log([...myArray, 4,5,6]);
